1bb2ff2e567ed95cb2fbfa7c22a1f0083b0d8c01
Jan Soendermann <jan.soendermann+git@gmail.com>
2016-11-08 20:52:54 +0100

    Add MongoAdapter to index.js exports

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-var HotaruServer = require('./lib/HotaruServer');
+var HotaruServer = require('./lib/HotaruServer').default;
+var MongoAdapter = require('./lib/MongoAdapter').default;
 
-module.exports = { HotaruServer };
+module.exports = { HotaruServer: HotaruServer, MongoAdapter: MongoAdapter};
